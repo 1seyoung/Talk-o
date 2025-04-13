c8141b396fca165fa4b0afd4a771767be9ff11ff
1seyoung <akdlrnal0705@gmail.com>
2025-04-14 00:30:02 +0900

    chore : init spring project

---
+++ src/main/java/com/talko/TalkoApplication.java
@@ -0,0 +1,13 @@
+package com.talko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TalkoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/talko/TalkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
